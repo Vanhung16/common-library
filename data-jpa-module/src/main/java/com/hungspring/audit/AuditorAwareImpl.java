8d1158209f0a04a7f5b70155b75501a0d21a4231
Nguyen Van Hung <hungnguyenvan209@gmail.com>
2023-11-26 23:10:44 +0700

    build lib

--- data-jpa-module/src/main/java/com/hungspring/audit/AuditorAwareImpl.java
+++ data-jpa-module/src/main/java/com/hungspring/audit/AuditorAwareImpl.java
@@ -1,6 +1,5 @@
 package com.hungspring.audit;
 
-import com.hungspring.tracing.IUserTracingExtractor;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -10,7 +9,6 @@ import java.util.Optional;
 public class AuditorAwareImpl implements AuditorAware<Object> {
     private static final String ANONYMOUS = "anonymousUser";
     private static final String NO_AUTHENTICATION = "noAuthentication";
-    private IUserTracingExtractor iUserTracingExtractor;
 
     @Override
     public Optional<Object> getCurrentAuditor() {
@@ -21,7 +19,8 @@ public class AuditorAwareImpl implements AuditorAware<Object> {
             if (authentication.getPrincipal().equals(ANONYMOUS)) {
                 return Optional.of(ANONYMOUS);
             } else {
-                return Optional.of(authentication).map(item -> iUserTracingExtractor.getUserId(authentication));
+                return null;
+//                return Optional.of(authentication).map(item -> iUserTracingExtractor.getUserId(authentication));
             }
         }
     }
